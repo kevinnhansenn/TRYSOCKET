771babc466ea2c2842747920af0c8cdde2dd4ee5
kevinnhansenn <57480445+kevinnhansenn@users.noreply.github.com>
2020-03-30 13:34:56 +0700

    Modify db

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const MONGODB_URL = process.env.MONGODB_URL
 
-mongoose.connect('mongodb://127.0.0.1:27017/socket', {
+mongoose.connect(MONGODB_URL, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
